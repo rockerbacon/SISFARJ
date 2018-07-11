dd84d21063e65b1f655eb33b63aa6dd14b7fda27
rockerbacon <mp8andrade@hotmail.com>
2018-07-11 18:05:17 -0300

    Implementado login

--- src/database/DbBatch.java
+++ src/database/DbBatch.java
@@ -2,14 +2,17 @@ package database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class DbBatch {
 	
 	public static void upDatabase (Connection con) throws SQLException {
 		Mapper mapper = new Mapper(con);
-		
+		Usuario secretario = new Usuario("admin", (byte)0, "admin");
+			
 		//ordem da criacao tem importancia
 		mapper.create(Associacao.class);
+		mapper.create(Usuario.class);
 		mapper.create(Atleta.class);
 		mapper.create(FormatoProva.class);
 		mapper.create(Classe.class);
@@ -18,6 +21,8 @@ public class DbBatch {
 		mapper.create(Local.class);
 		mapper.create(Competicao.class);
 		mapper.create(ProvaCompeticao.class);
+		
+		mapper.create(secretario);
 	}
 	
 	public static void dropDatabase (Connection con) throws SQLException {
@@ -32,8 +37,40 @@ public class DbBatch {
 		mapper.delete(Classe.class);
 		mapper.delete(FormatoProva.class);
 		mapper.delete(Atleta.class);
+		mapper.delete(Usuario.class);
 		mapper.delete(Associacao.class);
 		
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		Connection con = null;
+		
+		System.out.print(">");
+		String cmd = scn.nextLine();
+		scn.close();
+		try {
+			con = DbConnection.connect();
+			switch (cmd.toUpperCase()) {
+				case "UPDATABASE":
+					upDatabase(con);
+				break;
+				case "DROPDATABASE":
+					dropDatabase(con);
+				break;
+				default:
+					System.out.println("Commando nao reconhecido");
+			}
+			System.out.println("Command executado com exito");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null) con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 }
