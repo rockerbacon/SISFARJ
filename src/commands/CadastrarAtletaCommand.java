254b04a974b478fbed9e0020afa57898537a58ca
rockerbacon <mp8andrade@hotmail.com>
2018-07-12 00:07:08 -0300

    consertada livraria do tomcat8

--- src/commands/CadastrarAtletaCommand.java
+++ src/commands/CadastrarAtletaCommand.java
@@ -1,6 +1,8 @@
 package commands;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import database.DbConnection;
 import receivers.Secretario;
 import receivers.ValidationReceiver;
 
@@ -28,14 +31,18 @@ public class CadastrarAtletaCommand implements Command {
 	private HttpServletResponse response;
 	
 	public CadastrarAtletaCommand(HttpServletRequest request, HttpServletResponse response) {
+		Connection con = null;
+		String errMsg = null;
 		try {
 			
 			SimpleDateFormat dt = new SimpleDateFormat("dd/MM/yyyy");
 			String dataOf;
 			String dataNasc;
 			String dataEntrada;
+			
+			con = DbConnection.connect();
 			this.numero = Integer.parseInt(request.getParameter("numero"));
-			System.out.println(this.numero);
+			//System.out.println(this.numero);
 			dataOf = request.getParameter("data_oficio");
 			this.data_oficio = dt.parse(dataOf);
 			this.nome = request.getParameter("nome");
@@ -44,30 +51,31 @@ public class CadastrarAtletaCommand implements Command {
 			dataEntrada = request.getParameter("data_entrada");
 			this.data_entrada = dt.parse(dataEntrada);
 			this.matricula_atleta = Integer.parseInt(request.getParameter("matricula_atleta"));
-			System.out.println(this.matricula_atleta);
+			//System.out.println(this.matricula_atleta);
 			this.categoria = request.getParameter("categoria");
 			this.comprovante_pagamento = Integer.parseInt(request.getParameter("comprovante_pagamento"));
 			//this.comprovante_pagamento = 10;
 			//System.out.println(this.comprovante_pagamento);
-			this.receiver = new Secretario();
+			this.receiver = new Secretario(con);
 			this.request = request;
 			this.response = response;
 			
 		} catch (NumberFormatException e) {
-			try {
-				request.setAttribute("errorMsg", "Passagem de caracteres em campo numerico");
-				request.getRequestDispatcher("/error.jsp").forward(request, response);
-
-			} catch (IOException|ServletException e2) {
-				e2.printStackTrace();
-			}
+			errMsg = "Passagem de caracteres em campo numerico";
 		} catch (ParseException e) {
+			errMsg = "Data invalida";
+		} catch (SQLException e) {
+			errMsg = "Nao foi possivel conectar ao database";
+		
+		} finally {
 			try {
-				request.setAttribute("errorMsg", "Data invalida");
-				request.getRequestDispatcher("/error.jsp").forward(request, response);
-
-			} catch (IOException|ServletException e2) {
-				e2.printStackTrace();
+				if (con != null) con.close();
+				if (errMsg != null) {
+					request.setAttribute("errorMsg", errMsg);
+					request.getRequestDispatcher("/error.jsp").forward(request, response);
+				}
+			} catch (SQLException|ServletException|IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
